ff456b1660358ba0b406394894c9b6e653ac7821
1liochka1 <110454012+1liochka1@users.noreply.github.com>
2023-06-11 20:39:59 +0300

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ delay - от и до скольки секунд между кошельками
 chain from - из какой сети сообщение - ТОЛЬКО ИЗ BSC И POLYGON !!!
 сhain to - куда
 '''
-
+import random
 
 with open("keys.txt", "r") as f:
     keys = [row.strip() for row in f]
